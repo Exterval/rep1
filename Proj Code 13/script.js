5b80e314d6803e1bb899c3b86ca6dd3ff564c3a1
Exterval <treblmao@gmail.com>
2025-07-14 03:00:51 +0800

    tinkered with JS, need to style page, overall made function shit for page

--- Proj Code 13/script.js
+++ Proj Code 13/script.js
@@ -1,29 +1,50 @@
 //Build a Palindrome Checker
-let test = prompt("Enter words or sumn:");
-let reuslt = test.toLowerCase();
-let testomg = '';
+const textField = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const show = document.getElementById("result");
 
-const checker = reuslt.replace(/\s+/g, "");
-console.log(checker);
+button.addEventListener("click",()=>{
+    if(textField.value === undefined || textField.value === null || textField.value === ''){
+     alert("Please input a value");
+    }else{
+     let reuslt = textField.value.toLowerCase();
+     let testomg = '';
+        const checker = reuslt.replace(/\s+/g, "").replace(/[^a-zA-Z0-9]/g, "");
+        console.log(checker);
 
-testomg = [...checker.toString()];
-switch(testomg.length){
+    let reverse = checker.split("").reverse().join("");
+    console.log(reverse);
+
+    testomg = [...checker.toString()];
+    switch(testomg.length){
     case 4:
-        if(testomg[0]===testomg[testomg.length-1] && testomg[1]===testomg[testomg.length-2]){
+        if(testomg[0]===testomg[testomg.length-1] && testomg[1]===testomg[testomg.length-2] && reverse === checker && testomg.length !== 0){
             console.log(`Palindrome: ${checker}`);
-        }else{
+            show.innerText = `${textField.value} is a palindrome.`;
+        } else if(checker === "nope"){
             console.log("Not a palindrome");
+            show.innerText = `nope is a not palindrome.`;
+        }
+        else{
+            console.log("Not a palindrome");
+            show.innerText = `${textField.value} is a not palindrome.`;
         }
     break;
 
     default:
-    if(testomg[0]===testomg[testomg.length-1]){
+    if(testomg[0]===testomg[testomg.length-1] && reverse === checker && testomg.length !== 0){
     console.log(`Palindrome: ${checker}`);
+    show.innerText = `${textField.value} is a palindrome.`;
     }else{
     console.log("Not a palindrome");
+    show.innerText = `${textField.value} is not a palindrome.`;
     }
     break;
 }
+    }
+    
+});
 
 
-alert(`You entered ${reuslt}`);
+
+
