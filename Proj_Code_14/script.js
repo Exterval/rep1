b820935dc13e9074ad16e903bf6417ad0fa01a6d
Exterval <treblmao@gmail.com>
2025-07-14 16:17:59 +0800

    updated w/ modal

--- Proj_Code_14/script.js
+++ Proj_Code_14/script.js
@@ -1,1 +1,11 @@
 //Logic behind notes app, use CRUD Bbro pls
+
+function addNote(){
+    document.getElementById("modal").style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
