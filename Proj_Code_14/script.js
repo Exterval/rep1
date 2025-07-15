2b9b50d8427af4ecdff5b26442151824590f301a
Exterval <treblmao@gmail.com>
2025-07-15 19:45:37 +0800

    added functionality

--- Proj_Code_14/script.js
+++ Proj_Code_14/script.js
@@ -1,7 +1,44 @@
 //Logic behind notes app, use CRUD Bbro pls
 
+//array for added notes
+let notes = [];
+let noteIndex = 0;
+
+//submit on modal
+function submitNote(){
+  if(!document.getElementById('textNotesTitle').value || !document.getElementById('textNotes').value){
+    alert("Enter a value for each.");
+    return;
+  }else{
+  notes.push({
+    id:`${document.getElementById('textNotesTitle').value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    name: document.getElementById('textNotesTitle').value,
+    desc: document.getElementById('textNotes').value
+  });
+}
+  modal.style.display = "none";
+  checkForNotes();
+  document.getElementById('textNotesTitle').value = '';
+  document.getElementById('textNotes').value = '';
+
+}
+
 function addNote(){
     document.getElementById("modal").style.display = "block";
+}
+
+function checkForNotes(){
+  if(notes.length === 0){
+    console.err("No notes were found.");
+    return;
+  } else{
+    notes.forEach(({id, name, desc}, index) =>{ 
+      console.log(`Note found at index[${index}] is:
+        ID: ${id},
+        name: ${name}
+        description: ${desc} `);
+    });
+  }
 }
 
 window.onclick = function(event) {
