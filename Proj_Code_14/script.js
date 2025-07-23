8bb8a24ce553001112f895ed1057948cb3e88f1b
Exterval <treblmao@gmail.com>
2025-07-23 18:49:54 +0800

    added deleting function, only missing edit function

--- Proj_Code_14/script.js
+++ Proj_Code_14/script.js
@@ -9,13 +9,16 @@ function submitNote(){
   if(!document.getElementById('textNotesTitle').value || !document.getElementById('textNotes').value){
     alert("Enter a value for each.");
     return;
-  }else{
-  notes.push({
+  }
+
+  
+
+    notes.push({
     id:`${document.getElementById('textNotesTitle').value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
     name: document.getElementById('textNotesTitle').value,
     desc: document.getElementById('textNotes').value
   });
-}
+
   modal.style.display = "none";
   checkForNotes();
   document.getElementById('textNotesTitle').value = '';
@@ -24,6 +27,7 @@ function submitNote(){
 }
 
 function addNote(){
+  document.getElementById('confirmBtn').innerText = "Confirm";
     document.getElementById("modal").style.display = "block";
 }
 
@@ -45,23 +49,31 @@ function checkForNotes(){
 //function for updating
 function updateNotes(){
   document.getElementById('container').innerHTML  = "";
-  notes.forEach(({name, desc})=>{
+  notes.forEach(({id, name, desc})=>{
     document.getElementById('container').innerHTML  += `
       <h2>${name}</h2>
       <p>${desc}</p>
-
+      <button onclick="editNote(id)" class="noteBtn" id="${id}">Edit</button><button onclick = "deleteNote(id)" class="noteBtn" id="${id}">Delete</button>
     `
   });
 }
 
 //function for editing notes, remember to include the Id of the user's desired choice.
-function editNote(){
-
+function editNote(ID){
+      const noteFound = notes.find(notes => notes.id === ID);
+      document.getElementById('textNotesTitle').value = noteFound.name;
+      document.getElementById('textNotes').value = noteFound.desc;
+      document.getElementById('confirmBtn').innerText = "Edit";
+      document.getElementById("modal").style.display = "block";
 }
 
 //function for deleting notes, remember to include the Id of the user's desired choice.
-function deleteNote(){
-
+function deleteNote(ID){
+  let index = notes.findIndex(notes => notes.id === ID);
+  if(index !== -1){
+   notes.splice(index, 1); 
+  }
+  updateNotes();
 }
 
 window.onclick = function(event) {
