d9a8952915f77ba70c134eeec4f70776888114c8
Exterval <treblmao@gmail.com>
2025-08-25 18:31:36 +0800

    regex checking

--- Proj_Code_18/src/index.js
+++ Proj_Code_18/src/index.js
@@ -6,16 +6,30 @@
 const input = document.getElementById('user-input');
 const clear = document.getElementById('clear-btn');
 const check =  document.getElementById('check-btn');
-const result = document.getElementById('result-div');
+const result = document.createElement('p');
+
+// US NUMBERS = (XXX) YYY-ZZZZ
+
+const regex = /^(1\\s?)?(\([0-9]{3}\)|[0-9]{3})([-]?)([0-9]{3})([-]?)([0-9]{4})/;
+
+function isValid(){
+    const test = `1 1111111111`;
+    console.log(test);
+    console.log(regex.test(test));
+}
 
 check.addEventListener("click", ()=>{
-    if(input.value === '') alert("Please provide a phone number");
+    if(input.value === '') return alert("Please provide a phone number");
     else{
-
+        isValid();
     }
+    result.id = 'results-div';
+    result.textContent = 'testing';
+    document.body.appendChild(result);
 });
 
 clear.addEventListener("click", ()=>{
     input.value = '';
-    result.value = '';
+    if(!document.body.contains(result)) return;
+    else document.body.removeChild(result);
 })
