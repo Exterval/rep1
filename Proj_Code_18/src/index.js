14e67f68a30e8636618fe36b57fc307761e2acfe
Exterval <treblmao@gmail.com>
2025-08-26 20:53:47 +0800

    website done, to be revised in later versions

--- Proj_Code_18/src/index.js
+++ Proj_Code_18/src/index.js
@@ -6,30 +6,25 @@
 const input = document.getElementById('user-input');
 const clear = document.getElementById('clear-btn');
 const check =  document.getElementById('check-btn');
+const resultDiv = document.getElementById('results-div');
 const result = document.createElement('p');
-
 // US NUMBERS = (XXX) YYY-ZZZZ
 
-const regex = /^(1\\s?)?(\([0-9]{3}\)|[0-9]{3})([-]?)([0-9]{3})([-]?)([0-9]{4})/;
+const regex = /^(1\s?)?(\([0-9]{3}\)|[0-9]{3})(\s?[-]?\s?)([0-9]{3})(\s?[-]?\s?)([0-9]{4})$/;
 
-function isValid(){
-    const test = `1 1111111111`;
-    console.log(test);
-    console.log(regex.test(test));
+function isValid(inp){
+    console.log(inp);
+    return regex.test(inp);
 }
 
 check.addEventListener("click", ()=>{
     if(input.value === '') return alert("Please provide a phone number");
-    else{
-        isValid();
-    }
-    result.id = 'results-div';
-    result.textContent = 'testing';
-    document.body.appendChild(result);
+    result.textContent = isValid(input.value) ? `Valid US Number: ${input.value}` : `Invalid US Number: ${input.value}`;
+    resultDiv.appendChild(result);
 });
 
 clear.addEventListener("click", ()=>{
     input.value = '';
-    if(!document.body.contains(result)) return;
-    else document.body.removeChild(result);
+    if(resultDiv.textContent === '') return;
+    else resultDiv.textContent = '';
 })
